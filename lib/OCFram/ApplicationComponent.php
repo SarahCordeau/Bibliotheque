c9a2b97eb3a96b04d6d9b26485e800576e0080ee
SarahCordeau <cordeau.sarah@gmail.com>
2016-05-18 16:49:03 +0200

    Chapter 4: end

---
+++ lib/OCFram/ApplicationComponent.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace OCFram;
+
+/**
+ * Description of ApplicationComponent
+ *
+ */
+abstract class ApplicationComponent {
+    protected $app;
+    
+    public function __construct(Application $app) {
+        $this->app = $app;
+    }
+    
+    public function app() {
+        return $this->app;
+    }
+    
+}
